895dcec1947ffa37024a947fb2d38216ddab9a72
zbyn3k <coding707@mailbox.org>
2022-11-27 21:47:23 +0100

    Fetched phone details from the given API and added them to DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
-// write your code here
+const BASE_URL = 'https://mate-academy.github.io/phone-catalogue-static/api';
+
+const request = (url) => {
+  return fetch(`${BASE_URL}${url}`)
+    .then((response) => {
+      if (!response.ok) {
+        setTimeout(() => {
+          return Promise.reject(
+            new Error(`${response.status} - the endpoint does not exist`)
+          );
+        }, 5000);
+      }
+
+      if (!response.headers.get('content-type').includes('application/json')) {
+        setTimeout(() => {
+          return Promise.reject(
+            new Error(`Content-type is not application/json`)
+          );
+        }, 5000);
+      }
+
+      return response.json();
+    })
+    .catch((error) => setTimeout(Promise.reject(new Error(error)), 5000));
+};
+
+const getPhones = () => request('/phones.json');
+
+const getPhonesDetails = () => {
+  const list = document.createElement('ul');
+
+  getPhones().then((phones) => {
+    phones.forEach((phone) => {
+      const item = document.createElement('li');
+
+      item.innerText = phone.name;
+      list.append(item);
+    });
+  });
+
+  document.body.append(list);
+};
+
+getPhonesDetails();
